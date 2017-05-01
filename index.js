894cf58d3b48e0da9113933a7b151f4a4b696b7c
William Polenz <wpolenz@live.com>
2017-04-30 21:44:23 -0400

    Making the donation page pretty! First commit with images and some css, index.css index.html

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-﻿
+﻿window.addEventListener("keyup", setupCustomInput);
+
+function setupCustomInput(){
+	let c = document.getElementById('customInput');
+	let x1 = document.getElementById('username');
+	let x2 = document.getElementById('message');
+
+	// pad username with whitspace to the right by at most
+	// 50 characters
+	let padded = (x1.value+"                                                  ").slice(0,50);						
+	
+	c.value = padded + x2.value;
+}
