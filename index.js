2a73cb9cdb6111ba72d8e13992e0dcd6cdfb5f9b
William Polenz <wpolenz@live.com>
2017-12-06 21:22:52 -0500

    MemeURL can be used to edit custom field

--- index.js
+++ index.js
@@ -6,12 +6,19 @@
 		let c = document.getElementById('customInput');
 		let x1 = document.getElementById('username');
 		let x2 = document.getElementById('message');
+		let x3 = document.getElementById('memeURL');
 
 		// pad username with whitspace to the right by at most
 		// 50 characters
-		let padded = (x1.value+"                                                  ").slice(0,50);						
+		let padded = (x1.value+"                                                  ").slice(0,30);						
 		
-		c.value = padded + x2.value;
+		let x4 = padded + x2.value;
+		
+		let memeLen = x3.value.length;
+
+		x4 = x4.slice(0, 256 - memeLen);
+
+		c.value = x4 + x3.value;
 	}
 
 })();
